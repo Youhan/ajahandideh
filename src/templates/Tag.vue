6d9f3d9be1c2479fb9794a488b2cd50fc23e9fcd
Alireza Jahandideh <ar.jahandideh@gmail.com>
2020-10-10 20:58:15 -0500

    fix tag page

--- src/templates/Tag.vue
+++ src/templates/Tag.vue
@@ -1,11 +1,15 @@
 <template>
   <Layout>
-    <h1 class="tag-title text-center space-bottom">
-      # {{ $page.tag.title }}
-    </h1>
+    <div slot="header"></div>
 
-    <div class="posts">
-      <PostCard v-for="edge in $page.tag.belongsTo.edges" :key="edge.node.id" :post="edge.node"/>
+    <h1 class="font-display text-3xl text-white"># {{ $page.tag.title }}</h1>
+
+    <div class="flex mt-10">
+      <PostCard
+        v-for="edge in $page.tag.belongsTo.edges"
+        :key="edge.node.id"
+        :post="edge.node"
+      />
     </div>
   </Layout>
 </template>
@@ -33,21 +37,15 @@ query Tag ($id: ID!) {
 </page-query>
 
 <script>
-import Author from '~/components/Author.vue'
-import PostCard from '~/components/PostCard.vue'
+// import Author from "~/components/Author.vue";
+import PostCard from "~/components/PostCard.vue";
 
 export default {
   components: {
-    Author,
-    PostCard
+    // Author,
+    PostCard,
   },
-  metaInfo: {
-    title: 'Hello, world!'
-  }
-}
+};
 </script>
 
-<style lang="scss">
-
-</style>
-
+<style lang="scss"></style>
